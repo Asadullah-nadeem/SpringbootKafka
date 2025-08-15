88bc06bec54e6b15e1d89696cec2325316b1321a
Muhammad <asadullahnadeem48@gmail.com>
2025-08-15 11:15:09 +0530

    add code

--- src/main/java/org/example/nadeem/config/kafkaProducer_Config.java
+++ src/main/java/org/example/nadeem/config/kafkaProducer_Config.java
@@ -17,8 +17,10 @@ public class kafkaProducer_Config {
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
+        String configValue = ID_Config.CONFIG.getValue();
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, configValue);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
